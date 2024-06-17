8270e9e48db793db30a8223d1c6486ff65614784
harikrishnan2193 <harikrishnank2193@gmail.com>
2024-06-17 11:37:29 +0530

    event back-end finalised

--- DB/connection.js
+++ DB/connection.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose')
 const connection_string = process.env.CONNECTION_STRING
 
 mongoose.connect(connection_string).then(()=>{
-    console.log('mongoose connected successfully');
+    console.log('Mongodb Connect Succesfulliy');
 }).catch((err)=>{
-    console.log(err);
+    console.log(`Mongodb Connection Failed ${err}`);
 })
